a5ff8f49585a24e8282dc6b4cd47364fa4092369
kionling <djvdjv209@gmail.com>
2024-03-28 02:22:23 -0700

    Imported main script from a different repository due to a security issue (Address leaked) and now binded them to an environment variable. Application is functional

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests 
+import json
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+api_key = os.environ.get('API_KEY')
+destination = os.environ.get('destination')
+origin = os.environ.get('origins')
+
+url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+
+params = {
+    'destinations': {destination},
+'origins': {origin}, 
+    'units': 'imperial',
+    'key': api_key
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    duration = data['rows'][0]['elements'][0]['duration']['text']
+    distance = data['rows'][0]['elements'][0]['distance']['text']
+else:
+    print("Error: ", response.status_code)
+
+
