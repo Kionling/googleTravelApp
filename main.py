20fef4b1a7d45644b76ecf1a8b470f79af057778
Daniel Jauregui <djvdjv209@gmail.com>
2024-03-31 23:47:09 -0700

    defined a function that sends an email out containing the distance and duration information from the matrix API

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import json
 import os
 from dotenv import load_dotenv
 import time
+import smtplib, ssl
 
 load_dotenv()
+
 
 api_key = os.environ.get('API_KEY')
 destination = os.environ.get('destination')
@@ -24,9 +26,23 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     data = response.json()
     duration = data['rows'][0]['elements'][0]['duration']['text']
-    distance = data['rows'][0]['elements'][0]['distance']['text']   
+    distance = data['rows'][0]['elements'][0]['distance']['text']
 else:
     print("Error: ", response.status_code)
 
 
-print(distance)
+def send_email():
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = os.environ.get('sender_email')  # Enter your address
+    receiver_email = os.environ.get('receiver_email')  # Enter receiver address
+    password = os.environ.get('password')
+    message = f"""\
+    Subject: Distance and Time
+
+    The distance between the two locations is {distance} and the time taken to travel is {duration}."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
