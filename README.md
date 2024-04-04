a3f0304fc3bb6e06a5bdd8f516e3cd8934bda3bd
kionling <djvdjv209@gmail.com>
2024-04-04 00:35:02 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Google Matrix API Travel App
+Automating our daily commute information enables the user to schedule and organize their life more efficiently. This application uses the Google Matrix API to obtain distance and duration information on a set route. The application then send that information via email to the user at the same time everyday. This application is useful for someone who commutes to work often and wants to know their commute conditions at a specific moment of the day, everyday. 
+
+
+# Technologies 
+* Python 
+* Google Matrix API 
+* Redmail 
+* python-scheduler
+* python-time
+* dotenv
+* requests 
+* os
+
+# Code Snippet
+Redmail Gmail setup 
+```
+gmail.send(
+    subject='Hello, World!',
+    receivers=[to],
+    text="duration: " + duration + " distance: " + distance
+)
+
+```
+Google Matrix Query
+```
+arams = {
+    'destinations': {destination},
+'origins': {origin}, 
+    'units': 'imperial',
+    'key': api_key
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    duration = data['rows'][0]['elements'][0]['duration']['text']
+    distance = data['rows'][0]['elements'][0]['distance']['text']
+    print(duration, distance)
+else:
+    print("Error: ", response.status_code)
+
+```
+
+
+# Author
+* (Daniel Jauregui)[https://kionling.herokuapp.com]
+
+# Acknowledgements 
+* Redmail
+* Google Matrix API
+
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 import os
 from dotenv import load_dotenv
 import time
-import smtplib, ssl
+from redmail import EmailSender
+from redmail import gmail
+
 
 load_dotenv()
-
 
 api_key = os.environ.get('API_KEY')
 destination = os.environ.get('destination')
@@ -29,22 +30,18 @@ if response.status_code == 200:
     data = response.json()
     duration = data['rows'][0]['elements'][0]['duration']['text']
     distance = data['rows'][0]['elements'][0]['distance']['text']
+    print(duration, distance)
 else:
     print("Error: ", response.status_code)
 
 
-def send_email():
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = os.environ.get('sender_email')  # Enter your address
-    receiver_email = os.environ.get('receiver_email')  # Enter receiver address
-    password = os.environ.get('password')
-    message = f"""\
-    Subject: Distance and Time
+gmail.username = sender # Your Gmail address
+gmail.password = password
 
-    The distance between the two locations is {distance} and the time taken to travel is {duration}."""
+# And then you can send emails
+gmail.send(
+    subject='Hello, World!',
+    receivers=[to],
+    text="duration: " + duration + " distance: " + distance
+)
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
